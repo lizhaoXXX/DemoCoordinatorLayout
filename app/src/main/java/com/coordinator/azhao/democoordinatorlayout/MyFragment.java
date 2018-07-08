1ad1128c6f46b505fe11381f9c7aa7482a718355
azhao <li939858718@qq.com>
2018-07-09 02:40:05 +0800

    随机数

--- app/src/main/java/com/coordinator/azhao/democoordinatorlayout/MyFragment.java
+++ app/src/main/java/com/coordinator/azhao/democoordinatorlayout/MyFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 /**
  * @author azhao
  * @date 2018/7/9
@@ -20,8 +22,30 @@ public class MyFragment extends Fragment {
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		ImageView imageView = new ImageView(getActivity());
-		imageView.setImageResource(R.mipmap.a3);
-		imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+		Random random = new Random();
+		int i = random.nextInt(6);
+		switch (i){
+		    case 0:
+				imageView.setImageResource(R.mipmap.a1);
+		    break;
+		    case 1:
+				imageView.setImageResource(R.mipmap.a2);
+		    break;
+			case 2:
+				imageView.setImageResource(R.mipmap.a3);
+				break;
+			case 3:
+				imageView.setImageResource(R.mipmap.a6);
+				break;
+			case 4:
+				imageView.setImageResource(R.mipmap.a7);
+				break;
+		    default:
+				imageView.setImageResource(R.mipmap.a7);
+		        break;
+		}
+		
+		imageView.setScaleType(ImageView.ScaleType.CENTER);
 		return imageView;
 	}
 }
